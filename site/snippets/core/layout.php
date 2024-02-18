c503b27ca9d134afdb558dc61a0b2cd0f02e59f5
Linus Rogge <hello@linusrogge.com>
2024-02-18 20:21:06 +0100

    fix css import filename

--- site/snippets/core/layout.php
+++ site/snippets/core/layout.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?= $site->title() ?></title>
 
-  <?= css('public/assets/css/style.css') ?>
+  <?= css('public/assets/css/styles.css') ?>
 </head>
 <body>
   <?= snippet('core/nav') ?>
